d469d4827be17b4dbbdc7929acd7dbba6122ae7a
kodamayuto <yuto2001gyokugan@gmail.com>
2020-12-19 14:48:29 +0900

    2020-12-19

--- learning_pytorch_test1/android/app/src/main/kotlin/com/yuto/learning_pytorch_test1/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.yuto.learning_pytorch_test1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- new_my_widgets_app/android/app/src/main/kotlin/com/yuto/new_my_widgets_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.yuto.new_my_widgets_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
